b569a0971600122a60ac1f89f25cf3c5ece9dc9d
scott Tresor <scotttresor@gmail.com>
2022-06-20 17:09:12 +0200

    [FIX] roles displaying

--- app/Http/Controllers/Backend/RoleBackendController.php
+++ app/Http/Controllers/Backend/RoleBackendController.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Facades\Cache;
 
 class RoleBackendController extends Controller
 {
     public function index(): Renderable
     {
-        return view('backend.domain.roles.index');
+        $roles = Cache::remember('roles', 3600, function () {
+            return Role::query()
+               ->orderByDesc('created_at')
+               ->get();
+        });
+        return view('backend.domain.roles.index', compact('roles'));
     }
 }
--- resources/views/backend/domain/roles/index.blade.php
+++ resources/views/backend/domain/roles/index.blade.php
@@ -12,7 +12,45 @@
                     </div>
                 </div>
                 <div class="nk-block nk-block-lg">
-
+                    <table class="datatable-init nowrap nk-tb-list is-separate" data-auto-responsive="false">
+                        <thead>
+                        <tr class="nk-tb-item nk-tb-head">
+                            <th class="nk-tb-col tb-col-sm">
+                                <span>ROLE</span>
+                            </th>
+                            <th class="nk-tb-col text-center">
+                                <span>ACTIONS</span>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($roles as $role)
+                            <tr class="nk-tb-item">
+                                <td class="nk-tb-col">
+                                    <span class="tb-lead">{{ ucfirst($role->title) ?? "" }}</span>
+                                </td>
+                                <td class="nk-tb-col text-center">
+                                    <span class="tb-lead text-center">
+                                        <div class="d-flex">
+                                            <a href="{{ route('admins.roles.edit', $role->id) }}" class="btn btn-dim btn-primary btn-sm ml-1">
+                                                <em class="icon ni ni-edit"></em>
+                                            </a>
+                                            @if(auth()->user()->role_id == 6)
+                                                <form action="{{ route('admins.roles.destroy', $role->id) }}" method="POST" onsubmit="return confirm('Voulez vous supprimer');">
+                                                    @method('DELETE')
+                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                    <button type="submit" class="btn btn-dim btn-danger btn-sm">
+                                                        <em class="icon ni ni-trash"></em>
+                                                    </button>
+                                                </form>
+                                            @endif
+                                        </div>
+                                    </span>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
