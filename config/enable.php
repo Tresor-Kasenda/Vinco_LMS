6fdd7c967dfc75efccd2a977452f3ec4c4560a97
scott Tresor <scotttresor@gmail.com>
2022-07-29 16:01:30 +0200

    [FEAT] feat add enablex configuration

---
+++ config/enable.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ |
+ | Application: Sample App - 1 to 1 Video Call using EnableX Web Toolkit 1.3
+ | Version: 1.0.0
+ | This Sample Application demonstrates the use of Server APIs and Web Toolkit of EnableX platform to build
+ | an one to one Video Conference
+ | Configuration File - needs to be updated to meet your Application requirment
+ | NOTE! Your Application Server must be hosted on HTTPS.
+ | @author Scott-tresor <scotttresor@gmail.com>
+ */
+
+return [
+    'config' => [
+        'url' => env('ENABLE_URL', default: 'https://api.enablex.io/video/v2/'),
+        'app_id' => env('ENABLE_ID', ''),
+        'app_key' => env('ENABLE_KEY', '')
+    ]
+];
