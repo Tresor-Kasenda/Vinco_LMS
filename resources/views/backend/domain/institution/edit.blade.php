dddbc0a0eaae34599d1f298ed4dcffbf364c61e7
Dominique Youness <youness.dominique@gmail.com>
2022-08-08 10:11:56 +0200

    [STYLE] Change edit title in institution

--- resources/views/backend/domain/institution/edit.blade.php
+++ resources/views/backend/domain/institution/edit.blade.php
@@ -12,7 +12,7 @@
                     <div class="nk-block-head nk-block-head-sm">
                         <div class="nk-block-between">
                             <div class="nk-block-head-content">
-                                <h3 class="nk-block-title page-title">Edit Student</h3>
+                                <h3 class="nk-block-title page-title">Edit Institution</h3>
                             </div>
                             <div class="nk-block-head-content">
                                 <div class="toggle-wrap nk-block-tools-toggle">
