5f28d4080f336e03d9ab0b9ad115abe8ef93dea2
scott Tresor <scotttresor@gmail.com>
2022-08-22 15:43:04 +0200

    [FIx] rebase _ide and branch

--- database/migrations/2022_07_18_125720_create_exam_sessions_table.php
+++ database/migrations/2022_07_18_125720_create_exam_sessions_table.php
@@ -6,12 +6,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+return new class extends Migration
+{
     public function up()
     {
         Schema::create('exam_sessions', function (Blueprint $table) {
@@ -24,11 +20,6 @@ return new class extends Migration {
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('exam_sessions');
