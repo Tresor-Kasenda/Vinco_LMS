3d305c3aa06c0e18aaa3d077af508709b66d6ff6
Dominique Youness <youness.dominique@gmail.com>
2022-08-26 11:32:10 +0200

    [FIX] Lessons types id

---
+++ database/migrations/2022_08_08_083848_add_column_to_lesson_table.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\ExamSession;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->foreignIdFor(\App\Models\LessonType::class)
+                ->constrained()
+                ->cascadeOnDelete();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->dropForeignIdFor(\App\Models\LessonType::class);
+        });
+    }
+};
