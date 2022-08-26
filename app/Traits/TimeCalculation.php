84f465745fb69c2d6ed3843d29cb71a8abe73911
Dominique Youness <youness.dominique@gmail.com>
2022-08-26 11:49:36 +0200

    [FIX] Lecon type factory

--- app/Traits/TimeCalculation.php
+++ app/Traits/TimeCalculation.php
@@ -9,14 +9,14 @@ trait TimeCalculation
     public static function calculate($attributes): array
     {
         $dateOld = $attributes->input('date');
-        $time = $attributes->input('start_time');
+        $time = $attributes->input('startTime');
         $currentTime = strtotime(''.$dateOld.' '.$time.'');
         $hoursToAdd = -2;
         $secondsToAdd = $hoursToAdd * (60 * 60);
         $newTime = $currentTime + $secondsToAdd;
         $date = date('Y-m-d H:i:s', $newTime);
-        $time1 = $attributes->input('start_time');
-        $time2 = $attributes->input('end_time');
+        $time1 = $attributes->input('startTime');
+        $time2 = $attributes->input('endTime');
         $array1 = explode(':', $time1);
         $array2 = explode(':', $time2);
         $minutes1 = ($array1[0] * 60.0);
