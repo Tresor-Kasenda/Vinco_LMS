b44b38518afadbf4f4994d92a9b2d5f87a205931
Dominique Youness <youness.dominique@gmail.com>
2022-09-28 12:48:35 +0200

    [FEAT] Update information

--- resources/views/backend/domain/communication/library/index.blade.php
+++ resources/views/backend/domain/communication/library/index.blade.php
@@ -29,32 +29,25 @@
                         </tr>
                         </thead>
                         <tbody>
-{{--                        @foreach($notifications as $notification)--}}
-{{--                            <tr class="nk-tb-item text-center">--}}
-{{--                                <td class="nk-tb-col">--}}
-{{--                                    <span class="tb-lead">{{ ucfirst($notification->title) ?? "" }}</span>--}}
-{{--                                </td>--}}
-{{--                                <td class="nk-tb-col">--}}
-{{--                                    <span class="tb-lead">{{ $notification->content ?? "" }}</span>--}}
-{{--                                </td>--}}
-{{--                                <td class="nk-tb-col">--}}
-{{--                                    <span class="tb-lead">--}}
-{{--                                        <div class="d-flex">--}}
-{{--                                                <a href="{{ route('admins.communication.notification.edit', $notification->id) }}" class="btn btn-dim btn-primary btn-sm ml-1">--}}
-{{--                                                    <em class="icon ni ni-edit"></em>--}}
-{{--                                                </a>--}}
-{{--                                                <form action="{{ route('admins.communication.notification.destroy', $notification->id) }}" method="POST" onsubmit="return confirm('Voulez vous supprimer');">--}}
-{{--                                                    @method('DELETE')--}}
-{{--                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
-{{--                                                    <button type="submit" class="btn btn-dim btn-danger btn-sm">--}}
-{{--                                                        <em class="icon ni ni-trash"></em>--}}
-{{--                                                    </button>--}}
-{{--                                                </form>--}}
-{{--                                            </div>--}}
-{{--                                    </span>--}}
-{{--                                </td>--}}
-{{--                            </tr>--}}
-{{--                        @endforeach--}}
+                        @foreach($books as $book)
+                            <tr class="nk-tb-item text-center">
+                                <td class="nk-tb-col">
+                                    <span class="tb-lead">{{ ucfirst($book['thumbnailUrl']) ?? "" }}</span>
+                                </td>
+                                <td class="nk-tb-col">
+                                    <span class="tb-lead">{{ $book['title'] ?? "" }}</span>
+                                </td>
+                                <td class="nk-tb-col">
+                                    <span class="tb-lead">
+                                        <div class="d-flex">
+                                                <a href="" class="btn btn-dim btn-primary btn-sm ml-1">
+                                                    <em class="icon ni ni-eye"></em>
+                                                </a>
+                                            </div>
+                                    </span>
+                                </td>
+                            </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
